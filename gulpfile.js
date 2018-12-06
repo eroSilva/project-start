6acf278d9bb83e4f4679faabf61dc9dc1785192d
eroSilva <eronildo.silva@outlook.com>
2018-12-06 13:37:02 -0200

    Inclusão/adaptação de módulos

--- gulpfile.js
+++ gulpfile.js
@@ -1,54 +1,127 @@
-const gulp = require('gulp');
-const $ = require('gulp-load-plugins')();
-const browserSync = require('browser-sync').create();
+const gulp          = require('gulp');
+const $             = require('gulp-load-plugins')();
+const webpack       = require('webpack-stream');
+const browserSync   = require('browser-sync');
+const mqpacker      = require('css-mqpacker');
+const cssnano       = require('cssnano');
+const autoprefixer  = require('autoprefixer');
+
+const paths = {
+	extras: ['src/*.*', 'src/fonts/**/*'],
+	styles: 'src/styles/**/*.scss',
+	scripts: 'src/scripts/**/*.js',
+	images: 'src/images/**/*.{png,jpeg,jpg,svg,gif}',
+	dest: {
+		styles: 'dist/css',
+		scripts: 'dist/js',
+		images: 'dist/img',
+		extras: 'dist'
+	}
+}
 
 gulp.task('clean', () => {
-    return gulp.src('dist')
+    return gulp.src(paths.dest.extras)
       .pipe($.clean());
 });
 
-gulp.task('html', () => {
-    return gulp.src('src/*.html')
-        .pipe(gulp.dest('dist'));
+gulp.task('extras', () => {
+	return gulp.src(paths.extras, { base: 'src' })
+		.pipe(gulp.dest(paths.dest.extras));
 });
 
 gulp.task('styles', () => {
-    return gulp.src('src/styles/*.scss')
+	return gulp.src(paths.styles)
+		.pipe($.plumber())
+		.pipe( $.util.env.production ? $.util.noop() : $.sourcemaps.init() )
         .pipe(
             $.sass({
-                outputStyle: 'compressed'
-            }).on('error', $.sass.logError))
-        .pipe(gulp.dest('dist/css'));
+				outputStyle: $.util.env.production ? 'compressed' : 'nested',
+				errLogToConsole: true,
+				includePaths: [
+					'node_modules',
+					'src/styles'
+				]
+			}).on('error', $.sass.logError)
+		)
+		.pipe(
+			$.postcss([
+				mqpacker({ sort: true }),
+				cssnano({
+					autoprefixer: false,
+					reduceIdents: false
+				})
+			])
+		)
+		.pipe($.postcss([autoprefixer()]))
+		.pipe($.sourcemaps.write('.'))
+		.pipe(gulp.dest(paths.dest.styles));
 });
 
 gulp.task('scripts', () => {
-    return gulp.src('src/scripts/**/*.js')
+    return gulp.src(paths.scripts)
 		.pipe($.eslint())
 		.pipe($.eslint.format())
-		.pipe($.babel())
-        .pipe(gulp.dest('dist/js'));
+		.pipe(webpack({
+			mode: $.util.env.production ? 'production' : 'development',
+			output: {
+				filename: 'app.min.js'
+			},
+			resolve: {
+				modules: [
+					'src/scripts',
+					'node_modules'
+				],
+			},
+			module: {
+				rules: [
+					{
+						test: /\.m?js$/,
+						exclude: /(node_modules)/,
+						use: {
+							loader: 'babel-loader',
+							options: {
+								presets: ['@babel/preset-env']
+							}
+					  }
+					}
+				]
+			},
+			plugins: [],
+			devtool: $.util.env.production ? false : '#source-map'
+		}))
+        .pipe(gulp.dest(paths.dest.scripts));
 });
 
 gulp.task('images', () => {
-    return gulp.src('src/images/*')
-		.pipe($.imagemin())
-        .pipe(gulp.dest('dist/images'));
+    return gulp.src(paths.images)
+		.pipe($.imagemin({
+			optimizationLevel: $.util.env.production ? 5 : 1,
+			progressive: true,
+			interlaced: true,
+			svgoPlugins: [{ removeViewBox: true }]
+		}))
+        .pipe(gulp.dest(paths.dest.images));
 });
 
-gulp.task('watch', ['images', 'scripts', 'styles', 'html'], () => {
-	gulp.watch('src/scripts/**/*.js', ['scripts']);
-	gulp.watch('src/styles/**/*.scss', ['styles']);
-	gulp.watch('src/images/**/*', ['images']);
-	gulp.watch('src/**/*.html', ['html']);
+gulp.task('watch', ['scripts', 'styles', 'images', 'extras'], () => {
+	gulp.watch(paths.scripts, ['scripts']);
+	gulp.watch(paths.styles, ['styles']);
+	gulp.watch(paths.images, ['images']);
+	gulp.watch(paths.extras, ['extras']);
 });
 
-gulp.task('serve', ['watch'], () => browserSync.init({
+gulp.task('serve', ['watch'], () => browserSync({
 	files: ['dist/**', '!dist/**/*.map'],
-	server: './dist'
+	server: {
+		baseDir: ['dist']
+	},
+	open: !$.util.env.no
 }));
 
-gulp.task('default', ['serve']);
+gulp.task('default', ['clean'], () => gulp.start('serve'));
 
 gulp.task('build', ['clean'], () => {
-    gulp.start('images', 'scripts', 'styles', 'html');
+	$.util.env.production = true;
+
+    gulp.start('images', 'scripts', 'styles', 'extras');
 });
